0cd767387bfb4bcf7db77cc8ba003045581f89da
Bingo Bobo <matmazur90@gmail.com>
2019-01-08 11:11:39 +0100

    first commit

---
+++ src/main/java/com/matmazur/springvalidationboot/SpringValidationBootApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.springvalidationboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationBootApplication.class, args);
+    }
+
+}
+
